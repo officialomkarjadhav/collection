3238977854e2f2711efdc9c89753ac4f6f3201dc
omkar jadhav <officialomkarjadhav@gmail.com>
2023-03-10 05:10:14 +0000

    task1 updated

---
+++ task1.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+class task1{
+public static void main(String[] args) {
+    ArrayList <String> color=new ArrayList<>();
+    color.add("red");
+    color.add("white");
+    color.add("green");
+    color.add("blue");
+    System.out.println(color);
+}
+}
